2de80059f305ba5c364138649f4c7ac4a0a5801f
groy <g.royplisson@gmail.com>
2020-02-27 14:35:55 +0100

    ft_putchar avec width et - reglee

--- func_c.c
+++ func_c.c
@@ -6,7 +6,7 @@
 /*   By: groy <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/14 11:52:49 by groy              #+#    #+#             */
-/*   Updated: 2020/02/27 14:28:45 by groy             ###   ########.fr       */
+/*   Updated: 2020/02/27 14:35:19 by groy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	func_c (int c, t_list *all)
 	char a;
 
 	if (all->minus == 0)
-		ft_blank(all->width);
+		ft_blank(all->width - 1);
 	a = (unsigned char)c;
 	write(1, &a, 1);
 	if (all->minus == 1)
-		ft_blank(all->width);
+		ft_blank(all->width - 1);
 	return (all->width + 1);
 }
